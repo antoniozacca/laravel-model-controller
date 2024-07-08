9ec08ad985a3cec505c7475ec13e8e5572d25c86
antoniozacca <antoniozaccaria057@gmail.com>
2024-07-08 15:55:57 +0200

    fix

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    $Movie = Movie::all();
-    return view('welcome');
+    public function index()
+    {
+
+        $Movies = Movie::all();
+        dd($Movies);
+        return view('welcome');
+    }
 }
